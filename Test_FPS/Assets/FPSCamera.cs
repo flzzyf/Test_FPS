75a37d292866c1765a2b18134a0f312125aee048
flzzyf <2981019122@qq.com>
2017-12-13 15:44:15 +0800

    Init

---
+++ Test_FPS/Assets/FPSCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCamera : MonoBehaviour
+{
+
+    public Transform target;
+
+    public Vector3 offset;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        transform.position = target.position + offset;
+
+    }
+}
