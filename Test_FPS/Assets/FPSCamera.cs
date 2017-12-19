97429e22e7f22eca75da443a9d4fe2adaaeb74a2
Flzzyf <flzzyf076@gmail.com>
2017-12-19 14:50:57 +0800

    武器模组

--- Test_FPS/Assets/FPSCamera.cs
+++ Test_FPS/Assets/FPSCamera.cs
@@ -1,23 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class FPSCamera : MonoBehaviour
 {
 
+    public float distance = 10;
+
     public Transform target;
 
-    public Vector3 offset;
+    float rotationX = 0.0f;
+    float rotationY = 0.0f;
 
-    void Start()
+    public float mouseSensitivity = 3;
+
+    public Vector2 yMinMax = new Vector2(-85, 85);
+
+    Vector3 rotationSmoothVelocity;
+    Vector3 currentRotation;
+
+    public float rotationSmoothTime = .1f;
+
+    void LateUpdate()
     {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
+        rotationX += mouseX;
+        rotationY -= mouseY;
+        //Y角度约束
+        rotationY = Mathf.Clamp(rotationY, yMinMax.x, yMinMax.y);
+        //旋转镜头
+        Vector3 rotation = new Vector3(rotationY, rotationX);
+
+        currentRotation = Vector3.SmoothDamp(currentRotation, rotation, ref rotationSmoothVelocity, rotationSmoothTime);
+
+        transform.eulerAngles = currentRotation;
+        //移动镜头
+        transform.position = target.position - transform.forward * distance;
     }
 
-    void Update()
-    {
-
-        transform.position = target.position + offset;
-
-    }
 }
